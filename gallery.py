c70c0b27eac3204bbab2fbe19237eb4ad85d4c0b
alan <leggitta3@gmail.com>
2017-07-24 09:05:17 -0700

    added first gallery entry. still need to add links

---
+++ gallery.py
@@ -0,0 +1,23 @@
+from bokeh.plotting import figure, output_file, show
+import numpy as np
+
+tau = 2 * np.pi
+# tau = 360
+N = 1000
+
+fig = figure(width=600, height=300)
+
+x = np.linspace(0, tau, N)
+sin_x = np.sin(x)
+cos_x = np.cos(x)
+
+for a in np.linspace(0, 1, 100):
+    fig.line(
+        x, a * cos_x, color='blue', line_width=2, legend='Cosine', alpha=a)
+    fig.line(
+        x, a * sin_x, color='red', line_width=2, legend='Sine', alpha=a)
+fig.legend.location = 'bottom_left'
+fig.xaxis.axis_label = 'Angle (Radians)'
+
+output_file('gallery.html')
+show(fig)
