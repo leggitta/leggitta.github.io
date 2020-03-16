fc223342e4acc33608c6639e74e77eb9c1acdec2
Alan Leggitt <leggitta3@gmail.com>
2020-03-15 20:50:02 -0400

    added original graphics

--- catmouse/main.js
+++ catmouse/main.js
@@ -5,18 +5,16 @@ var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
 
-var mouse = new Image();
-mouse.src = 'mouse.jpg';
-
-
 class Cat {
-    constructor(x, y, dx, dy, acc, img) {
+    constructor(x, y, dx, dy, width, height, acc, img) {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.acc = acc;
         this.img = img;
+        this.width = width;
+        this.height = height;
     }
     draw() {
         ctx.drawImage(this.img, this.x, this.y);
@@ -28,9 +26,11 @@ class Cat {
         // update velocity based on keypress
         if (rightPressed) {
             this.dx += this.acc;
+            this.img.src = 'cat.png';
         }
         if (leftPressed) {
             this.dx -= this.acc;
+            this.img.src = 'cat_r.png';
         }
         if (upPressed) {
             this.dy -= this.acc;
@@ -44,34 +44,43 @@ class Cat {
             this.dx = 0;
             this.x = 0;
         }
-        if (this.x > canvas.width-50) {
+        if (this.x > canvas.width - this.width) {
             this.dx = 0;
-            this.x = canvas.width-50;
+            this.x = canvas.width - this.width;
         }
         if (this.y < 0) {
             this.dy = 0;
             this.y = 0;
         }
-        if (this.y > canvas.height-50) {
+        if (this.y > canvas.height - this.height) {
             this.dy = 0;
-            this.y = canvas.height-50;
+            this.y = canvas.height - this.height;
         }
     }
-
+    compute_distance(object) {
+        var dist_x = this.x + this.width/2 - object.x - object.width/2
+        var dist_y = this.y + this.height/2 - object.y - object.height/2
+        var dist = Math.pow(dist_x, 2) + Math.pow(dist_y, 2)
+        return dist
+    }
 }
 var cat_img = new Image();
-cat_img.src = 'cat.jpeg';
-cat = new Cat(100, 100, 0, 0, 0.1, cat_img);
+cat_img.src = 'cat.png';
+cat = new Cat(100, 100, 0, 0, 138, 174, 0.1, cat_img);
 
 
 class Mouse {
-    constructor(x, y, speed, img) {
+    constructor(x, y, speed, width, height) {
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.dx = speed;
         this.dy = speed;
-        this.img = img;
+        this.width = width;
+        this.height = height;
+
+        this.img = new Image();
+        this.img.src = 'mouse.png';
     }
 
     draw() {
@@ -80,21 +89,25 @@ class Mouse {
         this.x += this.dx;
         this.y += this.dy;
 
+        if (this.dx > 0) {
+            this.img.src = 'mouse_r.png';
+        } else if (this.dx < 0) {
+            this.img.src = 'mouse.png';
+        }
+
         if (this.x < 0) {
             this.dx = this.speed;
         } else if (this.y < 0) {
             this.dy = this.speed;
-        } else if (this.x > canvas.width-30) {
+        } else if (this.x > canvas.width - this.width) {
             this.dx = -this.speed;
-        } else if (this.y > canvas.height-30) {
+        } else if (this.y > canvas.height - this.height) {
             this.dy = -this.speed;
         }
     }
 }
-var mouse_img = new Image();
-mouse_img.src = 'mouse.jpg';
-mouse = new Mouse(700, 500, 10, mouse_img);
-mouse2 = new Mouse(300, 300, 5, mouse_img);
+mouse = new Mouse(700, 500, 10, 72, 82);
+mouse2 = new Mouse(300, 300, 5, 72, 82);
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -103,11 +116,13 @@ function draw() {
     mouse2.draw();
 
     // win the game
-    if (Math.pow(cat.x - mouse.x, 2) + Math.pow(cat.y - mouse.y, 2) < 100) {
+    if (cat.compute_distance(mouse) < 1000) {
+        mouse.img.src = 'explode.png';
         mouse.dx = 0;
-        mouse.dy = 0;        
+        mouse.dy = 0;
     }
-    if (Math.pow(cat.x - mouse2.x, 2) + Math.pow(cat.y - mouse2.y, 2) < 100) {
+    if (cat.compute_distance(mouse2) < 1000) {
+        mouse2.img.src = 'explode.png';
         mouse2.dx = 0;
         mouse2.dy = 0;
     }
