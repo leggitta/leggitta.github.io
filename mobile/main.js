0ef0136fbc41b71346a2c23bf524a33a126b2dd3
Alan Leggitt <leggitta3@gmail.com>
2020-03-19 21:54:11 -0400

    mobile app

---
+++ mobile/main.js
@@ -0,0 +1,44 @@
+var canvas = document.getElementByID("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = 100;
+var y = 100;
+var rad = 10;
+
+function cb_touchstart(e) {
+}
+
+function cb_touchmove(e) {
+    e.preventDefault();
+
+    touches = e.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var idx = ongoingTouchIndexById(touches[i].identifier);
+        if (idx >= 0) {
+            x = touches[i].pageX;
+            y = touches[i].pageY;
+        }
+    }
+}
+
+
+function cb_touchcancel(e) {
+}
+
+function cb_touchend(e) {
+}
+
+document.addEventListener("touchstart", cb_touchstart);
+document.addEventListener("touchmove", cb_touchmove);
+document.addEventListener("touchcancel", cb_touchcancel);
+document.addEventListener("touchend", cb_touchend);
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, rad, 0, Math.PI*2);
+    ctx.fillStyle = "#00ff00";
+    ctx.closePath();
+}
+
+var interval = setInterval(draw, 10);
